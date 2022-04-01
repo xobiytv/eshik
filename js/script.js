b6d601164cc99580168eeeef014e3a6242eafbfa
Diyor <xobiytv.gmail.com>
2022-04-01 16:30:35 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage2(anythin){
+  document.querySelector('.xitec2').src = anythin;
+}
+function changeImage3(anythin){
+  document.querySelector('.xitec3').src = anythin;
+}
+function changeImage4(anythin){
+  document.querySelector('.xitec4').src = anythin;
+}
+function changeImage5(anythin){
+  document.querySelector('.xitec5').src = anythin;
+}
+function changeImage6(anything){
+  document.querySelector('.xitec6').src = anything;
+}
+function changeImage7(anythin){
+  document.querySelector('.xitec7').src = anythin;
+}
+function changeImage8(anythin){
+  document.querySelector('.xitec8').src = anythin;
+}
+function changeImage9(anythin){
+  document.querySelector('.xitec9').src = anythin;
+}
+function changeImage10(anythin){
+  document.querySelector('.xitec10').src = anythin;
+}
+function changeImage11(anything){
+  document.querySelector('.xitec11').src = anything;
+}
+function changeImage12(anythin){
+  document.querySelector('.xitec12').src = anythin;
+}
+function changeImage13(anythin){
+  document.querySelector('.xitec13').src = anythin;
+}
+function changeImage14(anythin){
+  document.querySelector('.xitec14').src = anythin;
+}
+function changeImage15(anythin){
+  document.querySelector('.xitec15').src = anythin;
+}
+
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
+function changeImage1(anything){
+  document.querySelector('.xitec1').src = anything;
+}
